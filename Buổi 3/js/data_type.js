59a9d85fbb30552b2d9d9294d3d5b36e00df0f57
TranPhuDinh <trandinhphu55555@gmail.com>
2024-11-03 11:32:59 +0700

    text

---
+++ Buổi 3/js/data_type.js
@@ -0,0 +1,29 @@
+// 1.Kieu du lieu nguyen thuy: Primitive data -----------
+// Luu vao vung nho RAM: Khi gan lai gia tri -> tao ra 1
+// Vung nho moi de luu value -> goi la nguyen thuy
+/**
+ * Number
+ * String
+ * Boolean
+ * Underfined (Khong su dung = )
+ * Null
+ * Symbol - ES5
+ */
+
+// console.log(typeof 1);
+// console.log(typeof null); // object
+
+// 2. Du lieu phuc tap: Conplex Data ----------------
+/**
+ * Function
+ * Object types(object,array)
+ */
+// var my_func = function () {
+  //  console.log("first");
+ // };
+ // console.log(typeof my_func); // function
+  // console.log(typeof new Array(10)); // object
+
+  // toan tu so sanh (!==,===) ---------------
+ // console.log("1" !== 1); // true
+  // console.log("1" === 1); // false
